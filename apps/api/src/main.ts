bbcabd986dc9d77efc4b2e41c3496b223bacef5e
Harshil <harshilshah562@yahoo.com>
2025-08-05 00:27:47 -0400

    The user seed can now login from frontend and now it is setup with CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,4 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { Logger } from '@nestjs/common';
@@ -11,12 +8,18 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200', // Allow Angular frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `Application is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
